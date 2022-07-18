a89cbaf3f7413ee7b3f5d92f890c1cb63932906a
Darian Alexis Ortiz <dortiz@weknowinc.com>
2022-07-18 16:34:27 -0300

    Examples for ajax modals added.

--- web/modules/custom/weknow_module/src/Controller/WeknowModuleController.php
+++ web/modules/custom/weknow_module/src/Controller/WeknowModuleController.php
@@ -27,7 +27,15 @@ class WeknowModuleController extends ControllerBase
         '#attributes' => [
           'class' => [
             'link-weKnow',
+            'use-ajax',
           ],
+          'data-dialog-type' => 'dialog',
+          'data-dialog-options' => json_encode(
+              [
+              'width' => 800,
+              'height' => 500,
+              ]
+          ),
         ],
         '#url' => \Drupal\Core\Url::fromRoute('weknow_module.dependent_dropdown'),
         ];
@@ -39,13 +47,25 @@ class WeknowModuleController extends ControllerBase
         '#attributes' => [
           'class' => [
             'link-weKnow',
+            'use-ajax',
           ],
+          'data-dialog-type' => 'modal',
+            'data-dialog-options' => json_encode(
+                [
+                'width' => 800,
+                'height' => 500,
+                ]
+            ),
         ],
         '#url' => \Drupal\Core\Url::fromRoute('weknow_module.submit_driven'),
         ];
         $build['#attached']['library'][] = 'weknow_module/weknow_module';
 
-        return $build;
+        return [
+          '#theme' => 'weknow',
+          '#form_link1' => $build['submit_driven'],
+          '#form_link2' => $build['dependent_dropdown'],
+        ];
     }
 
 }
