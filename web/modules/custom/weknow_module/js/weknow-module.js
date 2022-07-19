e562a1de0f0699bd0476f70ffc4e1c8308272b64
Darian Alexis Ortiz <dortiz@weknowinc.com>
2022-07-19 00:11:12 -0300

    Once function are fixed

--- web/modules/custom/weknow_module/js/weknow-module.js
+++ web/modules/custom/weknow_module/js/weknow-module.js
@@ -1,19 +1,29 @@
-
-
 (function ($, Drupal, drupalSettings) {
 
   Drupal.behaviors.weknowModule = {
 
     attach: function (context, settings) {
-      if (once($('body').hasClass('path-weknow-module'))) {
-          const messages = new Drupal.Message();
-          const messageId = messages.add('test message');
-          //messages.remove(messageId);
-          messages.add('test message', {type: 'warning'});
-          messages.add('test message', {type: 'error'});
-          // Clear ALL
-          //messages.clear();
-        }
-      }
+      once('message', 'body', context).forEach(function () {
+        const messages = new Drupal.Message();
+        const messageId = messages.add('There are no open modals!');
+        $(window).on('dialog:beforecreate', function (e, dialog, $element, settings) {
+          $('body').addClass('modal-dialog-open');
+        });
+        $(window).on('dialog:beforeclose', function (e, dialog, $element, settings) {
+          $('body').removeClass('modal-dialog-open');
+          messages.clear();
+          messages.add('Again, there are no open modals');
+        });
+        $('.link-weKnow').on('click', function () {
+          messages.remove(messageId);
+          messages.add('There is an open modal', {
+            type: 'warning'
+          });
+          messages.add('I am red but this is not really a mistake', {
+            type: 'error'
+          });
+        });
+      })
     }
+  }
 })(jQuery, Drupal, drupalSettings);
