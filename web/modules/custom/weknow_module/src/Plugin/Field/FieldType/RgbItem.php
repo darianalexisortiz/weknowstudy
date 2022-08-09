bffd742a642c303e602d4f88b93033aee005e143
Darian Alexis Ortiz <dortiz@weknowinc.com>
2022-08-09 11:12:55 -0300

    FieldType added

---
+++ web/modules/custom/weknow_module/src/Plugin/Field/FieldType/RgbItem.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\weknow_module\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'field_example_rgb' field type.
+ *
+ * @FieldType(
+ *   id = "field_example_rgb",
+ *   label = @Translation("Example Color RGB"),
+ *   category = @Translation("General"),
+ *   description = @Translation("Demonstrates a field composed of an RGB color."),
+ *   default_widget = "field_example_text",
+ *   default_formatter = "field_example_simple_text"
+ * )
+ *
+ * @DCG
+ * If you are implementing a single value field type you may want to inherit
+ * this class form some of the field type classes provided by Drupal core.
+ * Check out /core/lib/Drupal/Core/Field/Plugin/Field/FieldType directory for a
+ * list of available field type implementations.
+ */
+class RgbItem extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return [
+      'columns' => [
+        'value' => [
+          'type' => 'text',
+          'size' => 'tiny',
+          'not null' => FALSE,
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Hex value'));
+
+    return $properties;
+  }
+
+
+}
